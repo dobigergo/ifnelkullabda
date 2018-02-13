e2a6ac5156e321f3262d2c6c880e23ca63bbb967
dobigergo <36455111+dobigergo@users.noreply.github.com>
2018-02-13 20:11:55 +0100

    Add files via upload

---
+++ valtozocsere.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+int a=452;
+int b=854; 
+
+cout << "eredeti változók:\n" << a << endl << b << endl;
+
+a=a*b;
+b=a/b;
+a=a/b;
+
+cout << "megcserélt szorzással:\n" << a << endl << b << endl;
+
+a=a+b;
+b=a-b;
+a=a-b;
+
+cout << "visszacserélés összeadással:\n" << a << endl << b << endl;
+
+return 0;
+}
